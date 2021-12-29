044ad048aae1130b06a08b005a3a68d9cb99301e
Avaneet Agrahari <68479381+agraavi786@users.noreply.github.com>
2021-12-29 12:18:34 +0530

    Update Class3.java

--- Class3.java
+++ Class3.java
@@ -1,3 +1,15 @@
+//Design a class that can be used by a health care professional to keep track of a patient’s vital statistics. The following are the details.
+//Name of the class - Patient
+//Member Variables - patientName(String),height(double),width(double)
+//Member Function - double computeBMI()
+//The above method should compute the BMI and return the result.
+//The formula for computation of BMI is weight (in kg) ÷ height*height(in metres).
+//Create an object of the Patient class and check the results.
+
+
+
+
+
 package classObject;
 
 public class Class3 {
